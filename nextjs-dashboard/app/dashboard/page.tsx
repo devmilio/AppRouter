c15b50fa54a7ec88fa4c14fdfd609845719f2520
Emilio <emiliogagnonpro@gmail.com>
2025-01-30 18:06:48 -0500

    changed to Neon db

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -4,8 +4,9 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue } from '@/app/lib/data';
  
+const revenue = await fetchRevenue();
+
 export default async function Page() {
-  const revenue = await fetchRevenue();
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
