4a0f89c8fa560f31b6c69555ec7a443c732907ea
Emilio <emiliogagnonpro@gmail.com>
2025-02-06 18:12:32 -0500

    fixed error from chapter 6-7

--- nextjs-dashboard/app/dashboard/page.tsx
+++ nextjs-dashboard/app/dashboard/page.tsx
@@ -4,19 +4,19 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
 import { fetchRevenue,
          fetchLatestInvoices,
-         fetchCardData } from '@/app/lib/data';
+         fetchCardData, } from '@/app/lib/data';
 
- 
-const revenue = await fetchRevenue();
-const latestInvoices = await fetchLatestInvoices();
-const {
+
+
+export default async function Page() {
+  const revenue = await fetchRevenue();
+  const latestInvoices = await fetchLatestInvoices();
+  const {
     numberOfInvoices,
     numberOfCustomers,
     totalPaidInvoices,
     totalPendingInvoices,
   } = await fetchCardData();
-
-export default async function Page() {
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
